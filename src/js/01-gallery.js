13757a36cd320a8daf5b8c81c0d52af00df44594
Dmitriy Korolov <adrorecords@gmail.com>
2021-10-27 15:31:18 +0300

    task 01

---
+++ src/js/01-gallery.js
@@ -0,0 +1,21 @@
+
+import SimpleLightbox from 'simplelightbox';
+import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsString = galleryItems.map(item => 
+  `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+).join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryItemsString);
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'botton',
+});
+// console.log(galleryItems);
