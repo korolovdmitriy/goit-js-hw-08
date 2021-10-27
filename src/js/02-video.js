dc5504bd51bb6aa0daecdcc5bcd0a776e531b119
Dmitriy Korolov <adrorecords@gmail.com>
2021-10-27 16:29:39 +0300

    task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
 
+const player = new Player('vimeo-player', {
+    id: 236203659,
+    width: 640
+});
+
+player.on('timeupdate', throttle(onPlayerTimeUpdate, 1000));
+
+function onPlayerTimeUpdate(event) { localStorage.setItem('videoplayer-current-time', `${event.seconds}`) };
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(savedTime);
